5b0425c0bb2f39598370d74a923e6c5197673c18
raven-panda <ambroise.maire7@gmail.com>
2024-05-25 17:31:57 +0200

    add: added put and delete mappings to user controller

--- HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/user/UserController.java
+++ HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/user/UserController.java
@@ -1,5 +1,6 @@
 package fr.ravenpanda.hyperbudget.user;
 
+import fr.ravenpanda.hyperbudget.user_preference.UserPreferenceEntity;
 import fr.ravenpanda.hyperbudget.user_preference.UserPreferenceRepository;
 import fr.ravenpanda.hyperbudget.user_preference.UserPreferenceService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +34,37 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
-        userPreferenceRepository.save(userPreferenceService.toEntity(userDto.getUserPreference()));
         return ResponseEntity.ok(userService.toDto(userRepository.save(userService.toEntity(userDto))));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody UserDto userDto) {
+        UserEntity user = userRepository.findById(id).orElse(null);
+        if (user == null) return ResponseEntity.notFound().build();
+        UserPreferenceEntity userPreference = userPreferenceRepository.findById(user.getUserPreference().getId()).orElse(null);
+        if (userPreference == null) return ResponseEntity.notFound().build();
+
+        userPreference.setTheme(userPreferenceService.toEntity(userDto.getUserPreference()).getTheme());
+        userPreference.setPeriodType(userPreferenceService.toEntity(userDto.getUserPreference()).getPeriodType());
+        userPreference.setIsEditWarn(userPreferenceService.toEntity(userDto.getUserPreference()).getIsEditWarn());
+
+        userPreferenceRepository.save(userPreference);
+
+        user.setEmail(userDto.getEmail());
+        user.setUsername(userDto.getUsername());
+        user.setPassword(userDto.getPassword());
+
+        return ResponseEntity.ok(userService.toDto(userRepository.save(user)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id, @RequestParam String password) {
+        UserEntity user = userRepository.findById(id).orElse(null);
+        if (user == null) return ResponseEntity.notFound().build();
+
+        if (!user.getPassword().equals(password)) return ResponseEntity.badRequest().build();
+
+        userRepository.delete(user);
+        return ResponseEntity.ok().build();
     }
 }
