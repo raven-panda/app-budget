83d9bfe11026282f400102c83c1cd05a119ff840
Ambroise Maire <a.maire@kiwilab.fr3>
2024-06-08 16:39:37 +0200

    rollback: rollback spring project

--- HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/budget/BudgetRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.ravenpanda.hyperbudget.budget;
-
-import jakarta.persistence.Table;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Table(name = "budget")
-public interface BudgetRepository extends JpaRepository<BudgetEntity, Long>{
-}
--- HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/category/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.ravenpanda.hyperbudget.category;
-
-import jakarta.persistence.Table;
-import jdk.jfr.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Table(name = "category")
-public interface CategoryRepository extends JpaRepository<Category, Long>{
-}
