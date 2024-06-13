a0ae868dd3e151bb73f9e12f2e22b107a1631af6
Ambroise Maire <a.maire@kiwilab.fr3>
2024-06-13 18:27:15 +0200

    add: added config for env file

---
+++ HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/config/HyperBudgetConfig.java
@@ -0,0 +1,20 @@
+package fr.ravenpanda.hyperbudget.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class HyperBudgetConfig {
+
+    private final static String ENV_FILE_PATH = ".env";
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource(ENV_FILE_PATH));
+        return configurer;
+    }
+
+}
