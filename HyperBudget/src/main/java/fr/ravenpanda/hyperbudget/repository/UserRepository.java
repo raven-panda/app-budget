31b0463ad1b033b8c813b0090544292204a494ee
Ambroise Maire <a.maire@kiwilab.fr3>
2024-06-27 23:25:44 +0200

    edit: added repository annotation to user repository

--- HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/repository/UserRepository.java
+++ HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/repository/UserRepository.java
@@ -3,7 +3,9 @@ package fr.ravenpanda.hyperbudget.repository;
 import fr.ravenpanda.hyperbudget.model.User;
 import jakarta.persistence.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Table
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 }
