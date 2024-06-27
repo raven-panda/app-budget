b2a9be5a85c549f962f8e7cc1bde89de5003cde3
Ambroise Maire <a.maire@kiwilab.fr3>
2024-06-28 01:13:13 +0200

    fix: fixed env file in other profile than local

--- HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/config/HyperBudgetEnvConfig.java
+++ HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/config/HyperBudgetEnvConfig.java
@@ -1,19 +1,30 @@
 package fr.ravenpanda.hyperbudget.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.FileSystemResource;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 @Configuration
-public class HyperBudgetConfig {
+public class HyperBudgetEnvConfig {
 
     private final static String ENV_FILE_PATH = ".env";
+
+    @Value("${SPRING_PROFILES_ACTIVE}")
+    private static String ENVIRONMENT;
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-        configurer.setLocation(new FileSystemResource(ENV_FILE_PATH));
+        if (Objects.equals(ENVIRONMENT, "local")) {
+            configurer.setLocation(new FileSystemResource(ENV_FILE_PATH));
+        }
         return configurer;
     }
 
