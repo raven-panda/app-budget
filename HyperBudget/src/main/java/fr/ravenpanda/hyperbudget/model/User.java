2a93f5886a9f710d2868c6b9f851e6d7bbde192b
Ambroise Maire <a.maire@kiwilab.fr3>
2024-06-27 23:05:24 +0200

    add: created user model/entity

---
+++ HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/model/User.java
@@ -0,0 +1,54 @@
+package fr.ravenpanda.hyperbudget.model;
+
+import fr.ravenpanda.hyperbudget.common.list.PeriodTypeEnum;
+import fr.ravenpanda.hyperbudget.common.list.PreferredThemeEnum;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false, columnDefinition = "ENUM('LIGHT', 'DARK')")
+    private PreferredThemeEnum theme;
+
+    @Column(nullable = false, columnDefinition = "ENUM('MONTHLY', 'YEARLY')")
+    private PeriodTypeEnum periodType;
+
+    @Column(name = "edit_warn", nullable = false)
+    private Boolean isEditWarnEnabled;
+}
