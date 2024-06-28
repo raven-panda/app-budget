944e94a02d6b3638715a304eb644c07d60ae432b
Ambroise Maire <a.maire@kiwilab.fr3>
2024-06-29 00:59:16 +0200

    add: added default category enum

---
+++ HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/common/list/DefaultCategoryEnum.java
@@ -0,0 +1,15 @@
+package fr.ravenpanda.hyperbudget.common.list;
+
+public enum DefaultCategoryEnum {
+	INSURANCE,
+	TRANSPORT,
+	LEISURE,
+	FOOD,
+	DIGITAL,
+	HOUSING,
+	HEALTH,
+	TAXES,
+	ANIMALS,
+	CHILDREN,
+	OTHER
+}
