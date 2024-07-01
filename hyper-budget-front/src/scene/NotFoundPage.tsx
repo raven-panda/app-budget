ad8d125e5bfcab5ccdba928121c894d7afd212f5
raven-panda <ambroise.maire7@gmail.com>
2024-07-01 20:47:33 +0200

    add: added not found page

---
+++ hyper-budget-front/src/scene/NotFoundPage.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <h1 className="text-4xl font-bold">404 Not Found</h1>
+      <p className="text-lg">The page you are looking for does not exist.</p>
+    </div>
+  )
+}
