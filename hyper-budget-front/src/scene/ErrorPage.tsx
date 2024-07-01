b1ec1a129fd3a98dbf30215e9d3a60c7819d6a73
raven-panda <ambroise.maire7@gmail.com>
2024-07-01 21:45:15 +0200

    edit: edited error page

--- hyper-budget-front/src/scene/ErrorPage.tsx
+++ hyper-budget-front/src/scene/ErrorPage.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
 import { ErrorResponse, isRouteErrorResponse, useRouteError } from "react-router-dom";
-import NotFoundError from "../component/error/NotFoundError";
 
-export default function ErrorPage() {
+export default function ErrorPage({data}: {data?: "NOT_FOUND"|"NOT_ALLOWED"}) {
   const unknownError = useRouteError();
   let error: ErrorResponse|null = null;
+  const errorMessage: string =
+    data === "NOT_FOUND" ? "Page non trouvée" :
+    data === "NOT_ALLOWED" ? "Vous n'êtes pas autorisé à accéder à cette page"
+    : "Il semblerait qu'il y ait un problème avec cette page...";
 
   if (isRouteErrorResponse(unknownError)) {
     error = unknownError;
@@ -12,15 +14,14 @@ export default function ErrorPage() {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      {error?.status === 404 ? 
-        <NotFoundError />
-        :
-      <>
-        <h1 className="text-4xl font-bold">Oups !</h1>
-        <p>Il semblerait qu'il y ait un problème avec cette page...</p>
-        <p><i>{error?.status + " " + error?.statusText}</i></p>
-        <p><i>{error?.data}</i></p>
-      </>}
+      <h1 className="text-4xl font-bold">Oups !</h1>
+      <p>{errorMessage}</p>
+      
+      {/**
+       * @TODO Show error details only in development
+       */}
+      <p><i>{(error?.status || "") + " " + (error?.statusText || "")}</i></p>
+      <p><i>{error?.data}</i></p>
     </div>
   );
   
