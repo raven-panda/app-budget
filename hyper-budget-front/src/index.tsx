15492d2cdf69aae08f749ebca46113476024c846
raven-panda <ambroise.maire7@gmail.com>
2024-07-01 20:48:58 +0200

    add: implemented react router, delete app file

--- hyper-budget-front/src/index.tsx
+++ hyper-budget-front/src/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import AppLayout from './Layout';
+import appRoutes, { AppRoute } from './routes';
+import NotFoundPage from './scene/NotFoundPage';
+
+export default function App() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NotFoundPage/>} />
+          <Route path="/" element={<AppLayout />}>
+            {appRoutes.map((route: AppRoute) => (
+              <Route key={route.path} path={route.path} element={route.element} />
+            ))}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
