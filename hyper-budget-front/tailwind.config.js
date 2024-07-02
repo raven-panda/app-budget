f33cee3a2fde3b2d1c016d06cc795bc87f38e044
Ambroise Maire <a.maire@kiwilab.fr3>
2024-07-02 19:25:47 +0200

    add: added font awesome package, added new tailwind vars

--- hyper-budget-front/tailwind.config.js
+++ hyper-budget-front/tailwind.config.js
@@ -6,16 +6,35 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
+        '10px': "10px",
         '24px': "24px"
-      }
-    },
-    colors: {
-      primary: {
-        light: "#9570FF"
       },
-      secondary: {
-        light: "#FFDEDE"
-      }
+      backgroundColor: {
+        primary: {
+          DEFAULT: "#9570FF",
+          light: "#9570FF"
+        },
+        secondary: {
+          DEFAULT: "#FFDEDE",
+          light: "#FFDEDE"
+        }
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#000000",
+          light: "#000000",
+          faded: "rgba(0, 0, 0, 0.5)"
+        },
+        secondary: {
+          DEFAULT: "#FFFFFF",
+          light: "#FFFFFF",
+          faded: "rgba(255, 255, 255, 0.5)"
+        },
+        important: {
+          DEFAULT: "#FF0000",
+          light: "#FF0000"
+        },
+      },
     },
   },
   plugins: [],
