84255576b196cc06ef1f4ae8f721cb42619a3c68
raven-panda <ambroise.maire7@gmail.com>
2024-07-03 22:41:57 +0200

    edit: removed content from home page

--- hyper-budget-front/src/scene/dashboard/DashboardHomePage.tsx
+++ hyper-budget-front/src/scene/dashboard/DashboardHomePage.tsx
@@ -1,9 +1,5 @@
-import DashboardHeader from "@component/dashboard/DashboardHeader";
-
 export default function DashboardHomePage() {
   return (
-    <>
-      <DashboardHeader />
-    </>
+    <></>
   )
 }
