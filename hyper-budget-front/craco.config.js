578d53f96448b9dd26b8fab68b21b870fe8dbcd9
raven-panda <ambroise.maire7@gmail.com>
2024-07-03 20:07:34 +0200

    add: added craco, implemented import aliases

---
+++ hyper-budget-front/craco.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@component': path.resolve(__dirname, 'src/component'),
+      '@scene': path.resolve(__dirname, 'src/scene'),
+      '@service': path.resolve(__dirname, 'src/service'),
+      '@data': path.resolve(__dirname, 'data'),
+    },
+  },
+};
