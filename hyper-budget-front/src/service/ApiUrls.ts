2b70015558c74a7815c4ca6420c9eb34ec061434
raven-panda <ambroise.maire7@gmail.com>
2024-07-03 22:55:01 +0200

    edit: fixed env, edited env

--- hyper-budget-front/src/service/ApiUrls.ts
+++ hyper-budget-front/src/service/ApiUrls.ts
@@ -1,5 +1,5 @@
 export default class ApiUrls {
-  public static readonly fixtureEnabled = false;
+  public static readonly FIXTURE_ENABLED = process.env.REACT_APP_FIXTURE_ENABLED && JSON.parse(process.env.REACT_APP_FIXTURE_ENABLED);
   private static readonly BASE_URL = process.env.API_URL;
 
   public static readonly USER = `${this.BASE_URL}/user`;
--- hyper-budget-front/src/service/UserService.ts
+++ hyper-budget-front/src/service/UserService.ts
@@ -3,8 +3,8 @@ import IUserDto from "./model/IUserDto";
 import Fixture from "./fixture/index";
 
 export default class UserService {
-  private static user: IUserDto = ApiUrls.fixtureEnabled && process.env.FIXTURE_USER_ID ?
-    Fixture.getUserById(parseInt(process.env.FIXTURE_USER_ID))
+  private static user: IUserDto = ApiUrls.FIXTURE_ENABLED && process.env.REACT_APP_FIXTURE_USER_ID ?
+    Fixture.getUserById(parseInt(process.env.REACT_APP_FIXTURE_USER_ID))
     : {} as IUserDto;
 
   private static url: string = ApiUrls.USER;
