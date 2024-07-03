cdf83aead15576822b6cb461a8529295ae348c5b
raven-panda <ambroise.maire7@gmail.com>
2024-07-03 23:30:45 +0200

    edit: changed style container & dashboardheader, added children to dashboardheader

--- hyper-budget-front/src/component/container/Container.tsx
+++ hyper-budget-front/src/component/container/Container.tsx
@@ -2,7 +2,7 @@ import { twMerge } from "tailwind-merge";
 
 export default function Container({children, background}: {children: JSX.Element | JSX.Element[], background?: string}) {
   return (
-    <div className={twMerge("p-24px", background)}>
+    <div className={twMerge("p-10px", background)}>
       {children}
     </div>
   );
--- hyper-budget-front/src/component/dashboard/DashboardHeader.tsx
+++ hyper-budget-front/src/component/dashboard/DashboardHeader.tsx
@@ -4,30 +4,36 @@ import { faBell, faUser } from '@fortawesome/free-solid-svg-icons'
 
 interface DashboardHeaderProps {
   username: string;
+  children?: React.ReactNode;
 }
-export default function DashboardHeader({ username }: DashboardHeaderProps) {
-  const notifications = 688;
+export default function DashboardHeader({ children, username }: DashboardHeaderProps) {
+  const notifications = 0;
 
   return (
     <header>
-      <Container background="flex items-center bg-secondary">
-        <div className="flex p-10px rounded-full bg-primary">
-          <FontAwesomeIcon icon={faUser} className="px-[2px] text-3xl" color="white"/>
+      <Container background="bg-secondary">
+        <div className="flex items-center">
+          <div className="flex p-10px rounded-full bg-primary">
+            <FontAwesomeIcon icon={faUser} className="px-[2px] text-3xl" color="white"/>
+          </div>
+          <div className="ml-10px">
+            <h1 className="text-base font-regular">Bonjour, {username} !</h1>
+              <p className="hidden xs:block text-xs">
+                <span className="text-primary-faded">
+                  { notifications > 0 ? 
+                    <>
+                      Vous avez <span className="text-red-600">{notifications}</span> notification{notifications > 1 ? "s": ""} en attente
+                    </> : "Vous n’avez aucune nouvelle notification"
+                  }
+                </span>
+              </p>
+          </div>
+          <div className="flex ml-auto p-10px rounded-full">
+            <FontAwesomeIcon icon={faBell} className="px-[2px] text-3xl" color="black"/>
+          </div>
         </div>
-        <div className="ml-10px">
-          <h1 className="text-lg font-bold">Bonjour, {username} !</h1>
-          <p className="text-xs">
-            <span className="text-primary-faded">
-              { notifications > 0 ? 
-                <>
-                  Vous avez <span className="text-red-600">{notifications}</span> notification{notifications > 1 ? "s": ""} en attente
-                </> : "Vous n’avez aucune nouvelle notification"
-              }
-            </span>
-          </p>
-        </div>
-        <div className="flex ml-auto p-10px rounded-full">
-          <FontAwesomeIcon icon={faBell} className="px-[2px] text-3xl" color="black"/>
+        <div className="mt-10px">
+          {children}
         </div>
       </Container>
     </header>
