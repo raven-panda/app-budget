7f54c97f639bbc8f1adfdf1dedcb1ed036276bc4
raven-panda <ambroise.maire7@gmail.com>
2024-07-04 00:41:07 +0200

    edit: using adapt user in user service, update user fixture

--- hyper-budget-front/src/service/fixture/index.ts
+++ hyper-budget-front/src/service/fixture/index.ts
@@ -1,13 +1,14 @@
-import IUserDto from "@service/model/IUserDto";
+import IUserDto from "src/model/dto/IUserDto";
 import User1 from "./user1.json";
+import UserDao from "@service/dao/UserDao";
 
 export default class Fixture {
   public static getUserById(id: number): IUserDto {
     switch (id) {
       case 1:
-        return User1;
+        return UserDao.adaptUser(User1);
       default:
-        return User1;
+        return UserDao.adaptUser(User1);
     }
   }
 }
