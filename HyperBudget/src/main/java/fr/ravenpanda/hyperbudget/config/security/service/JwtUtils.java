39d55d9cf28414b4d30a1017efedcc3906362434
raven-panda <ambroise.maire7@gmail.com>
2024-08-10 03:18:57 +0200

    edit/dev-auth: edited jwt components to use email instead of username

--- HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/config/security/service/JwtUtils.java
+++ HyperBudget/src/main/java/fr/ravenpanda/hyperbudget/config/security/service/JwtUtils.java
@@ -33,7 +33,7 @@ public class JwtUtils {
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
         return Jwts.builder()
-                .setSubject((userPrincipal.getUsername()))
+                .setSubject((userPrincipal.getEmail()))
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(key(), SignatureAlgorithm.HS256)
@@ -44,7 +44,7 @@ public class JwtUtils {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
-    public String getUserNameFromJwtToken(String token) {
+    public String getEmailFromJwtToken(String token) {
         return Jwts.parserBuilder().setSigningKey(key()).build()
                 .parseClaimsJws(token).getBody().getSubject();
     }
